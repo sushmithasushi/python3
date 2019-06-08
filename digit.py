bcd7c04e00ccf78cf3ec9708764f3b3207bd114f
balajimanikandanm <mkbalaji1998@gmail.com>
2019-06-08 09:31:11 +0530

    Update digit.py

--- digit.py
+++ digit.py
@@ -5,9 +5,9 @@ for i in sets:
   s1.append(i)
 b1=0
 u1=0
-for i in range(0,n-1,1):
+for i in range(0,n1-1,1):
   b1=0
-  for j in range(0,n,1):
+  for j in range(0,n1,1):
     if i!=j:
       if s1[i]==s1[j]:
         b1=b1+1
