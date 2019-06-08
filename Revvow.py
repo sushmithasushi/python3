1b67a877270da76e6797d39efed8e42034ba392c
balajimanikandanm <mkbalaji1998@gmail.com>
2019-06-08 09:11:11 +0530

    Create Revvow.py

---
+++ Revvow.py
@@ -0,0 +1,7 @@
+y=int(input())
+a=input()
+b=a[::-1]
+for i in b:
+  if(i!='E' and i!='A' and i!='I' and i!='O' and i!='U' and i!='e' and i!='a' and i!='i' and i!='o' and i!='u':
+    print(i,end="")
+  
