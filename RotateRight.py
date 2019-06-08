961f728384311834726d7a23d2543b2aa802979d
balajimanikandanm <mkbalaji1998@gmail.com>
2019-06-08 09:05:04 +0530

    Update RotateRight.py

--- RotateRight.py
+++ RotateRight.py
@@ -1,5 +1,5 @@
-aa2,bb=map(int,input().split())
-bb=bb%aa2
-l12=list(map(int,input().split()))
-l22=l12[-b2:]+l12[:-b2]
-print(*l22)
+q,r=[int(x) for x in input().split()]
+l=[int(b) for b in input().split()]
+for i in range(r):
+  l.insert(0,1.pop())
+ print(*l)
