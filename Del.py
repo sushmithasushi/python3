af1ab373bc8b478b069a7260aaa5ffe0d4f985bf
balajimanikandanm <mkbalaji1998@gmail.com>
2019-06-11 20:31:15 +0530

    Update Del.py

--- Del.py
+++ Del.py
@@ -1,7 +1,7 @@
 from itertools import combinations
-string1,num1=map(int,input().split())
-num2=len(str(string1))
-a1=list(combinations(string(string1),num2-num1))
+string1,n1=map(int,input().split())
+n2=len(str(string1))
+a1=list(combinations(string(string1),n2-n1))
 a1=(sorted(a1))
-b="".join(a1[0])
-print(b)
+b1="".join(a1[0])
+print(b1)
