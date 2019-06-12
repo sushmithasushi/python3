811d2a8e5c9eb7d29dd53d3920092e3f9a884b81
balajimanikandanm <mkbalaji1998@gmail.com>
2019-06-12 15:26:18 +0530

    Update Len.py

--- Len.py
+++ Len.py
@@ -1,2 +1,12 @@
-in,i=input().split()
-print(len(i))
+inp=int(input())
+li=list(map(int,input().split()))
+m=[]
+c=1
+for i in range(n-1):
+	if li[i]<li[i+1]:
+		c+=1
+	else:
+		m.append(c)
+		c=1
+m.append(c)
+print(max(m))
