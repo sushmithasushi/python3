b70c014999f246cdde3a38f52b3aa1605a203996
balajimanikandanm <mkbalaji1998@gmail.com>
2019-06-12 13:45:17 +0530

    Update Arrayed.py

--- Arrayed.py
+++ Arrayed.py
@@ -1,16 +1,16 @@
-nn,mm=map(int,input().split())
-l=[]
+n1,m1=map(int,input().split())
+li=[]
 ui=0
 for i in range(n1):
-    l.append(list(map(int,input().split())))   
-for i in range(nn):
-    for j in range(mm-1):
-        for k in range(j+1,mm+1):
-            if l[i][j:k]==[1]*len(l[i][j:k]):
-                 if all(l[p+i][j:k]==[1]*len(l[p+i][j:k]) for p in range(len(l[i][j:k])-1)):
-                     if len(l[i][j:k])>ui:
-                        ui=len(l[i][j:k])
-if len(l)==1 and len(l[0])==1 and l[0][0]==1:
+    li.append(list(map(int,input().split())))   
+for i in range(n1):
+    for j in range(m1-1):
+        for k in range(j+1,m1+1):
+            if li[i][j:k]==[1]*len(li[i][j:k]):
+                 if all(li[p+i][j:k]==[1]*len(li[p+i][j:k]) for p in range(len(li[i][j:k])-1)):
+                     if len(li[i][j:k])>ui:
+                        ui=len(li[i][j:k])
+if len(li)==1 and len(li[0])==1 and l[0][0]==1:
     print(1)
 for i in range(ui):
     print(*[1]*ui) 
