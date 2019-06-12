c6c54a4e641be55eae8e0f60a250f1ba6cbf6a65
balajimanikandanm <mkbalaji1998@gmail.com>
2019-06-12 15:44:43 +0530

    Update Waysc.py

--- Waysc.py
+++ Waysc.py
@@ -1,14 +1,14 @@
-n1=int(input())
+n=int(input())
 list=[]
-for i1 in range(1,n1):
+for i in range(1,n):
     rev=0
-    temp=i1
+    temp=i
     while temp!=0:
         re=temp%10
         rev=rev+re
         temp=temp//10
-    if i1+rev==n1:
-        list.append(i1)
+    if i+rev==n:
+        list.append(i)
 print(len(list))
-for j1 in range(0,len(list)):
-    print(list[j1])
+for j in range(0,len(list)):
+    print(list[j])
