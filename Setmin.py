556ad7de9c20107522f7feb3ace6d6598d1f1487
balajimanikandanm <mkbalaji1998@gmail.com>
2019-06-12 09:18:49 +0530

    Update Setmin.py

--- Setmin.py
+++ Setmin.py
@@ -1,6 +1,6 @@
-n,n2=map(int,input().split())
+n1,n2=map(int,input().split())
 lis=list(map(int,input().split()))
 for i in range(n2):
-    u,v=map(int,input().split())
-    c=lis[u-1:v]
+    u1,v1=map(int,input().split())
+    c=lis[u1-1:v1]
     print(min(c))
