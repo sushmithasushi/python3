5b41f68bd65df54f06036496be1e94e012a38508
balajimanikandanm <mkbalaji1998@gmail.com>
2019-06-12 15:46:38 +0530

    Update Waysc.py

--- Waysc.py
+++ Waysc.py
@@ -7,7 +7,7 @@ for ii in range(1,num):
         re=temp%10
         reve=reve+re
         temp=temp//10
-    if ii+reve==n:
+    if ii+reve==num:
         list.append(ii)
 print(len(list))
 for jj in range(0,len(list)):
