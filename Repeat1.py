7e67177f2018987ad60712ac8f47562064a22931
balajimanikandanm <mkbalaji1998@gmail.com>
2019-06-12 15:18:07 +0530

    Update Repeat1.py

--- Repeat1.py
+++ Repeat1.py
@@ -5,9 +5,9 @@ l1=len(k1)
 for i in range(0,2**nu):
 	s1=bin(i1)[2::]
 	if len(s1)<l1:
-		a1.append([s1.count("1"),(l1-len(s1))*"0"+s1])
+		a.append([s1.count("1"),(l1-len(s1))*"0"+s1])
 	else:
-		a1.append([s1.count("1"),s1])
+		a.append([s1.count("1"),s1])
 a.sort()
 for i in range(0,len(a)):
 	print(a1[i1][1])
