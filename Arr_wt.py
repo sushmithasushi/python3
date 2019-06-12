3e3bea9974f8ee4cb33a0f47b418f7459e1c2505
balajimanikandanm <mkbalaji1998@gmail.com>
2019-06-12 15:36:08 +0530

    Update Arr_wt.py

--- Arr_wt.py
+++ Arr_wt.py
@@ -5,9 +5,9 @@ r1=[]
 c1=0
 for i in range(n1):
     x1=v1[i]/p1[i]
-    r1.append(x)
-while w1>=0 and len(r)>0:
-    mindex=r.index(max(r))
+    r1.append(x1)
+while w1>=0 and len(r1)>0:
+    mindex=r1.index(max(r1))
     if w1>=p1[mindex]:
         c1=c1+v1[mindex]
         w1=w1-p1[mindex]
