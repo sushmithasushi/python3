018b99eae7eba2f8bc96dbd4b28b3fafe1dfbdfc
balajimanikandanm <mkbalaji1998@gmail.com>
2019-06-13 12:13:11 +0530

    Update Splits.py

--- Splits.py
+++ Splits.py
@@ -1,8 +1,8 @@
 d1,e1=map(int,input().split())
-li=list(map(int,input().split()))
+li1=list(map(int,input().split()))
 if e1==1:
-    print(min(li))
+    print(min(li1))
 elif e1==2:
-    print(max(li[0],li[d-1]))
+    print(max(li1[0],li1[d1-1]))
 else:
-    print(max[li])
+    print(max[li1])
