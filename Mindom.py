fb97dc374489a6a2c7985f04eab519e3ce344c2e
balajimanikandanm <mkbalaji1998@gmail.com>
2019-06-13 09:23:03 +0530

    Update Mindom.py

--- Mindom.py
+++ Mindom.py
@@ -1,8 +1,8 @@
 import sys
-def cfreq(s) :
+def cfreq(s1) :
     dic1 = {}
-    for c1 in s1 :
-        dic1[c1] = dic1.get(c1,0) + 1
+    for c in s1 :
+        dic1[c] = dic1.get(c,0) + 1
     return dic1
 
 ss1 = input()
@@ -11,17 +11,17 @@ dic1 = cfreq(ss1)
 Lk1 = list(dic1.keys())
 
 for j1 in range(n1-2,-1,-1) :
-      for c1 in Lk1 :
+      for c in Lk1 :
         k1 = 0
         for i1 in range(0,n1-j1) :
             li1, ri1 = i1,j1+i1
             s21 = s1[li1:ri1 + 1]
             #print(c,s2)
-            if c1 in s21 :
+            if c in s21 :
                 k1 += 1
         if k1 == n1-j1 :
             #print('c,k',c,k)
-            c21 = c1
+            c21 = c
             k21 = k1
             len21 = j1+1
 print(len21)
