807dab22316efd4b3cfbf45412762f38d7c018f2
balajimanikandanm <mkbalaji1998@gmail.com>
2019-06-13 12:25:24 +0530

    Update Maxoflist.py

--- Maxoflist.py
+++ Maxoflist.py
@@ -1,9 +1,9 @@
 n1,a1,b1,c1=map(int,input().split())
 li1=list(map(int,input.split()))
-x=[]
-for i in range(0,len(li)):
-  for j in range(i,len(li)):
-    for k in range(j,len(li)):
-      x.append(a1*li[i]+b1*li[j]+c1*li[k])
-print(max(x))
+f=[]
+for i in range(0,len(li1)):
+  for j in range(i,len(li1)):
+    for k in range(j,len(li1)):
+      f.append(a1*li1[i]+b1*li1[j]+c1*li1[k])
+print(max(f))
       
