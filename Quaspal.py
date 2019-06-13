aabd3e7778841cb1b43de63a9054feb716fede2b
balajimanikandanm <mkbalaji1998@gmail.com>
2019-06-13 12:32:45 +0530

    Create Quaspal.py

---
+++ Quaspal.py
@@ -0,0 +1,21 @@
+numb=input()
+n1=numb[::-1]
+c1=0
+if numb==n1:
+	print("yes")
+else:
+	ss=numb[::-1]
+	for i in range(len(s)):
+		if ss[i]=="0":
+			s1=ss[i+1::]
+			s2=s1[::-1]
+			if s1==s2:
+				c1=c1+1
+			else:
+				continue
+		else:
+			break
+	if c1>0:
+		print("yes")
+	else:
+		print("no")
