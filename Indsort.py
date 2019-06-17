f514fce72f23628cc62233eaeff347ed84b133af
balajimanikandanm <mkbalaji1998@gmail.com>
2019-06-17 09:24:57 +0530

    Update Indsort.py

--- Indsort.py
+++ Indsort.py
@@ -1,12 +1,12 @@
 n1=int(input())
-li=[]
+li1=[]
 arr=list(map(int,input().split()))
 for i in range(0,n1):
   if arr[x]==x:
-    li.append(x)
-if len(li)==0:
+    li1.append(x)
+if len(li1)==0:
   print("-1")
-li.sort()
-for x in li:
+li1.sort()
+for x in li1:
   print(x,end="")
     
