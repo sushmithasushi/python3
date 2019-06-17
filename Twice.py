83ab1cd96d4a83b15aabb3c8066782fc4d6eb832
balajimanikandanm <mkbalaji1998@gmail.com>
2019-06-17 10:48:40 +0530

    Create Twice.py

---
+++ Twice.py
@@ -0,0 +1,14 @@
+n=int(input())
+a=list(map(int,input().split()))
+c=[]
+co=0
+for i in a:
+     if i not in c:
+          c.append(i)
+for j in c:
+     for k in a:
+          if(j==k):
+               co=co+1
+     if(co==1):
+          print(j)
+     count=0
