f9f77210717a5e951bddf4f2d18047e0af177c31
balajimanikandanm <mkbalaji1998@gmail.com>
2019-06-19 13:48:05 +0530

    Update Books.py

--- Books.py
+++ Books.py
@@ -1,10 +1,10 @@
-n,t=map(int,input().split())
-s=list(map(int,input().split()))
-c=0
-for i in s:
+n1,t1=map(int,input().split())
+s1=list(map(int,input().split()))
+c1=0
+for i in s1:
      t1=86400-i
      t=t-t1
-     c=c+1
+     c1=c1+1
      if t<=0:
           break  
-print(c)
+print(c1)
