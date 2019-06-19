1bbcdf76f1dc1a54b6ec96a47283c15a90b84c1a
balajimanikandanm <mkbalaji1998@gmail.com>
2019-06-19 09:09:23 +0530

    Update Thief.py

--- Thief.py
+++ Thief.py
@@ -1,21 +1,21 @@
-n,k = map(int,input().split())
-a = list(map(int,input().split()))
+n1,k = map(int,input().split())
+a1 = list(map(int,input().split()))
 b,c = 0,[]
-for i in range(0,len(a)):
+for i in range(0,len(a1)):
   t = i
-  for p in range(0,len(a)):
+  for p in range(0,len(a1)):
     for l in range(0,k):
       if l == k-1:
         try:
-          b += a[p+i]
+          b += a1[p+i]
         except IndexError:
             t = t-1
-            b +=a[t]
+            b +=a1[t]
       else:
-        b += a[i]
+        b += a1[i]
     c.append(b)
     b = 0
-for i in range(0,len(a),k):
-  b = sum(a[i:i+k])
+for i in range(0,len(a1),k):
+  b = sum(a1[i:i+k])
   c.append(b)
 print(*sorted(set(c)))
