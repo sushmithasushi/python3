bed91f910eac9965f8e4039a6cf37a89ad6c5f52
balajimanikandanm <mkbalaji1998@gmail.com>
2019-07-01 17:46:38 +0530

    Create Dhonigame.py

---
+++ Dhonigame.py
@@ -0,0 +1,14 @@
+n1=int(input())
+li=[]
+a=n1//2+n1
+for i in range(1,n1+1):
+    if i%2==0:
+        li.append(i)
+for i in range(1,n1+1):
+    if i%2!=0:
+        li.append(i)
+for i in range(1,n1+1):
+    if i%2==0:
+        li.append(i)
+print(a)
+print(*li)
