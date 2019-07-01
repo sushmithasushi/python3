4a1f4635cf2410e678e4c229fa8538dec14bb906
balajimanikandanm <mkbalaji1998@gmail.com>
2019-07-01 17:35:22 +0530

    Update Checkstr.py

--- Checkstr.py
+++ Checkstr.py
@@ -1,11 +1,9 @@
-a=input()
-b=input()
+a,b=map(str,input().split())
 count=0
 for i in range(0,len(a)):
     for j in range(0,len(b)):
         if a[i]==b[j]:
             count+=1
-    
 if count>=2:
     print("yes")
 else:
