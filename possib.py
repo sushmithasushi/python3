d19ef2389c007e629e0468b77f10bc5e02ddc132
balajimanikandanm <mkbalaji1998@gmail.com>
2019-07-12 12:06:19 +0530

    Update possib.py

--- possib.py
+++ possib.py
@@ -2,6 +2,6 @@ P1=int(input())
 M1=list(map(int,input().split()))
 for i in range(0,P1-2):
  for j in range(i+1,P1-1):
-  for k in range(j+1,1P):
+  for k in range(j+1,P1):
    if(M1[i]+M1[j]==M1[k]):
     print(M1[i], M1[j], M1[k])
